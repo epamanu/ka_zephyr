28879b4885617d12527d398413f7a5b57c1940cc
epamanu <epalemanu@gmail.com>
2020-07-02 17:45:10 -0400

    Menu Icon Change color according BG

--- script/fullmenu.js
+++ script/fullmenu.js
@@ -6,11 +6,11 @@ $(document).ready(function(){
 
 		if( $('.btn-menu i').attr('class') == 'ka-open_menu' ){
 
-			$('.btn-menu i').removeClass('ka-open_menu').addClass('ka-close_menu').css({'color':'#000'});
+			$('.btn-menu i').removeClass('ka-open_menu').addClass('ka-close_menu');
 			$('.full-menu').css({'right':'0'});
 
 		}else{
-			$('.btn-menu i').removeClass('ka-close_menu').addClass('ka-open_menu').css({'color':'#fff'});
+			$('.btn-menu i').removeClass('ka-close_menu').addClass('ka-open_menu');
 			$('.full-menu').css({'right':'-100%'});
 		}
 
